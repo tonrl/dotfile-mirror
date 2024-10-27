e77ace17410fc68af1498c48b613b6bc178ad1cd
tonrl <hoswt4s3321399@gmail.com>
2024-10-28 08:46:53 +0900

    Add scripts hypr

---
+++ .config/hypr/scripts/hyprlock-effect.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+current_hour=$(date +"%H")
+user_string="<span color='#b10efb'>$USER</span>"
+
+if [ "$current_hour" -ge 4 ] && [ "$current_hour" -lt 12 ]; then
+    echo "Guten Morgen, $user_string!"
+elif [ "$current_hour" -ge 12 ] && [ "$current_hour" -lt 18 ]; then
+    echo "Guten Tag, $user_string!"
+elif [ "$current_hour" -ge 18 ] && [ "$current_hour" -lt 22 ]; then
+    echo "Guten Abend, $user_string!"
+else
+    echo "Gute Nacht, $user_string!"
+fi

e8d004e1b84dfc9d1729da8546bf4f30d8b32280
tonrl <hoswt4s3321399@gmail.com>
2024-10-29 11:01:45 +0900

    Upload sway config

---
+++ .config/sway/lock_screen.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+swayidle \
+    timeout 10 'swaymsg "output * dpms off"' \
+    resume 'swaymsg "output * dpms on"' &
+
+# Locks the screen immediately
+swaylock \
+	--screenshots \
+	--clock \
+	--indicator \
+	--indicator-radius 100 \
+	--indicator-thickness 7 \
+	--effect-blur 7x5 \
+	--effect-vignette 0.5:0.5 \
+	--ring-color bb00cc \
+	--key-hl-color 880033 \
+	--line-color 00000000 \
+	--inside-color 00000088 \
+	--separator-color 00000000 \
+	--grace 1 \
+	--fade-in 0.8
+
+# Kills last background task so idle timer doesn't keep running
+kill %%

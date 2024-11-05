36e519265d1141c2982d2e18d6594ca8c4afe570
tonrl <hoswt4s3321399@gmail.com>
2024-11-05 12:11:38 +0900

    Update nvim

---
+++ .config/nvim/lua/schlau-nvim/plugin-lazy/dashboard.lua
@@ -0,0 +1,49 @@
+return {
+        'nvimdev/dashboard-nvim',
+        event = 'VimEnter',
+        config = function()
+                require('dashboard').setup {
+                        thme = 'hyper',
+                        config = {
+                                header = {
+                                        "",
+                                        "███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗",
+                                        "████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║",
+                                        "██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║",
+                                        "██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║",
+                                        "██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║",
+                                        "╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝",
+                                        "",
+
+                                },
+                                shortcut = {
+                                        {
+                                                desc = '󰊳 Update', group = '@property', action = 'Lazy update', key = 'u'
+                                        },
+                                        {
+                                                icon = ' ',
+                                                icon_hl = '@variable',
+                                                desc = 'Files',
+                                                group = 'Label',
+                                                action = 'Telescope find_files',
+                                                key = 'f',
+                                        },
+                                        {
+                                                desc = ' dotfiles',
+                                                group = 'Number',
+                                                action = 'Telescope dotfiles',
+                                                key = 'd',
+                                        },
+                                },
+                                packages = {
+                                        enable = true
+                                },
+                                footer = {
+                                        "",
+                                        " Emacs is an OS with bad editor, Vi is a masterpiece of elegance",
+                                },
+                        }
+                }
+        end,
+        dependencies = { {'nvim-tree/nvim-web-devicons'}}
+}

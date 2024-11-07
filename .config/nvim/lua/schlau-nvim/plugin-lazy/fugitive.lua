26bb6401cd7c1d9c64561a4bc7e01739c05222a7
tonrl <hoswt4s3321399@gmail.com>
2024-11-07 21:51:50 +0900

    Update nvim fonfig

--- .config/nvim/lua/schlau-nvim/plugin-lazy/fugitive.lua
+++ .config/nvim/lua/schlau-nvim/plugin-lazy/fugitive.lua
@@ -3,11 +3,11 @@ return {
     config = function()
         vim.keymap.set("n", "<leader>gs", vim.cmd.Git)
 
-        local ThePrimeagen_Fugitive = vim.api.nvim_create_augroup("ThePrimeagen_Fugitive", {})
+        local schlau_fugitive = vim.api.nvim_create_augroup("schlau_fugitive", {})
 
         local autocmd = vim.api.nvim_create_autocmd
         autocmd("BufWinEnter", {
-            group = ThePrimeagen_Fugitive,
+            group = schlau_fugitive,
             pattern = "*",
             callback = function()
                 if vim.bo.ft ~= "fugitive" then

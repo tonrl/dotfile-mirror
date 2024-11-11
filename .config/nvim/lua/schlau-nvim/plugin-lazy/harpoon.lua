4e81a874e738e2fc2a326e46c44e9ef016e554da
tonrl <hoswt4s3321399@gmail.com>
2024-11-11 19:56:49 +0900

    Fix error

--- .config/nvim/lua/schlau-nvim/plugin-lazy/harpoon.lua
+++ .config/nvim/lua/schlau-nvim/plugin-lazy/harpoon.lua
@@ -73,7 +73,7 @@ return {
                 vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
                 vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
                 vim.keymap.set("n", "<C-h>", function() harpoon:list():select(3) end)
-                vim.keymap.set("n", "<C-t", function() harpoon:list():select(4) end)
+                vim.keymap.set("n", "<C-t>", function() harpoon:list():select(4) end)
 
                 -- Toggle previous & next buffers stored within Harpoon list
                 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)

178ffe424edc0aa1e443ea7dcd86c801cce1e679
tonrl <hoswt4s3321399@gmail.com>
2024-11-11 09:07:25 +0900

    Update nvim config

--- .config/nvim/lua/schlau-nvim/plugin-lazy/telescope.lua
+++ .config/nvim/lua/schlau-nvim/plugin-lazy/telescope.lua
@@ -1,15 +1,15 @@
 return {
-    "nvim-telescope/telescope.nvim",
+        "nvim-telescope/telescope.nvim",
 
-    tag = "0.1.5",
+        tag = "0.1.5",
 
-    dependencies = {
-        "nvim-lua/plenary.nvim",
-        "nvim-telescope/telescope-ui-select.nvim"
-    },
+        dependencies = {
+                "nvim-lua/plenary.nvim",
+                "nvim-telescope/telescope-ui-select.nvim"
+        },
 
-    config = function()
-		require('telescope').setup({
+        config = function()
+                require('telescope').setup({
                         layout_config = {
                                 vertical = {
                                         width = 0.5,
@@ -24,9 +24,9 @@ return {
                         }
                 })
 
-		local builtin = require('telescope.builtin')
+                local builtin = require('telescope.builtin')
 
-		vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
+                vim.keymap.set('n', '<leader>pf', builtin.find_files, {})
                 vim.keymap.set('n', '<leader>gg', builtin.grep_string, {})
                 vim.keymap.set('n', '<leader>pg', builtin.live_grep, { desc = 'Telescope live grep' })
                 vim.keymap.set('n', '<leader>pb', builtin.buffers, { desc = 'Telescope buffers' })
@@ -34,13 +34,13 @@ return {
 
 
 
-		vim.keymap.set('n', '<leader>ps', function()
-			builtin.grep_string({ search = vim.fn.input("Grep > ") })
-		end)
-		vim.keymap.set('n', '<leader>sh', builtin.help_tags, {desc = '[S]earch [H]elp'})
-		-- Search for Neovim config file
-		vim.keymap.set('n', '<leader>sn', function()
-			builtin.find_files ({ cwd = vim.fn.stdpath 'config' })
+                vim.keymap.set('n', '<leader>ps', function()
+                        builtin.grep_string({ search = vim.fn.input("Grep > ") })
+                end)
+                vim.keymap.set('n', '<leader>sh', builtin.help_tags, {desc = '[S]earch [H]elp'})
+                -- Search for Neovim config file
+                vim.keymap.set('n', '<leader>sn', function()
+                        builtin.find_files ({ cwd = vim.fn.stdpath 'config' })
                 end, {desc = '[S]earch [N]eovim files'})
                 require("telescope").load_extension("ui-select")
 

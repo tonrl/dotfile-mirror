d8bea846ba755bf6afc0bd7eeac0fcd3da3fa808
tonrl <hoswt4s3321399@gmail.com>
2024-11-11 19:55:39 +0900

    Update nvim config

--- .config/nvim/lua/schlau-nvim/plugin-lazy/harpoon.lua
+++ .config/nvim/lua/schlau-nvim/plugin-lazy/harpoon.lua
@@ -70,10 +70,10 @@ return {
                 -- Keymap for Harpoon
                 vim.keymap.set("n", "<leader>a", function() harpoon:list():add() end)
                 -- vim.keymap.set("n", "<leader>d", function() harpoon:list():remove() end)
-                vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-                vim.keymap.set("n", "<C-t>", function() harpoon:list():select(2) end)
-                vim.keymap.set("n", "<C-n>", function() harpoon:list():select(3) end)
-                vim.keymap.set("n", "<C-s>", function() harpoon:list():select(4) end)
+                vim.keymap.set("n", "<C-n>", function() harpoon:list():select(1) end)
+                vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
+                vim.keymap.set("n", "<C-h>", function() harpoon:list():select(3) end)
+                vim.keymap.set("n", "<C-t", function() harpoon:list():select(4) end)
 
                 -- Toggle previous & next buffers stored within Harpoon list
                 vim.keymap.set("n", "<C-S-P>", function() harpoon:list():prev() end)

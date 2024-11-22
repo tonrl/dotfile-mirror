02844bae8e1b04030485823bd449e1a4910c5bc4
tonrl <hoswt4s3321399@gmail.com>
2024-11-22 12:00:35 +0900

    Minor fix neovim config

--- .config/nvim/lua/schlau-nvim/set.lua
+++ .config/nvim/lua/schlau-nvim/set.lua
@@ -29,8 +29,8 @@ vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
 vim.opt.undofile = true
 
 --vim.opt.scrolloff = 8
---vim.opt.isfname:append("@-@")
-vim.opt.signcolumn = "yes"
+vim.opt.isfname:append("@-@")
+vim.opt.signcolumn = "auto"
 
 
 vim.opt.colorcolumn = "80"

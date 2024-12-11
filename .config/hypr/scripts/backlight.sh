399e6d8486c72ede7daada5d227800a093a02c3c
tonrl <hoswt4s3321399@gmail.com>
2024-12-11 19:15:58 +0900

    Update Hyprland conf

--- .config/hypr/scripts/backlight.sh
+++ .config/hypr/scripts/backlight.sh
@@ -9,11 +9,12 @@ function get_brightness {
         max_brightness=$(brightnessctl max)
         echo $((brightness * 100 / max_brightness))
 }
+
 function send_notification {
 
         brightness=$(get_brightness)
         brightness=${brightness%%.*}
-        
+
         if [ "$brightness" -gt 0 ]; then
                 bar=$(seq -s "─" $(($brightness / 5)) | sed 's/[0-9]//g')"─"
         else
@@ -22,23 +23,50 @@ function send_notification {
 
         if [ "$brightness" -ge 80 ]; then
                 dunstify -a "System" -i "${high_icon}" -r 2593 -u low "Brightness: ${brightness}" "$bar" -t 2500
-        
+
         elif [ "$brightness" -ge 30 ]; then
                 dunstify -a "System" -i "${mid_icon}" -r 2593 -u low "Brightness: ${brightness}" "$bar" -t 2500
-        
+
         else
                 dunstify -a "System" -i "${low_icon}" -r 2593 -u low "Brightness: ${brightness}" "$bar" -t 2500
         fi  
 
 }
 
+function brightness_up {
+        brightness=$(get_brightness)
+        brightness=${brightness%%.*}
+
+        if [ "$brightness" -lt 1 ]; then
+                brightnessctl -qe s +1%
+        elif [ "$brightness" -lt 10 ]; then
+                brightnessctl -q s +1%
+        else
+                brightnessctl -q s +5%
+        fi
+}
+function brightness_down {
+        brightness=$(get_brightness)
+        brightness=${brightness%%.*}
+
+        if [ "$brightness" -le 1 ]; then
+                brightnessctl -qe s 1%-
+        elif [ "$brightness" -le 10 ]; then
+                brightnessctl -q s 1%-
+        else
+                brightnessctl -q s 5%-
+        fi
+}
+
 case $1 in
         up)
-                brightnessctl -q s +5%
+                brightness_up
                 send_notification
                 ;;
         down)
-                brightnessctl -q s 5%-
+                brightness_down
                 send_notification
                 ;;
 esac
+
+
--- .config/hypr/scripts/volume.sh
+++ .config/hypr/scripts/volume.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 icon_base="/usr/share/icons/ePapirus-Dark/32x32/"
-icon_volume="$icon_base/panel/audio-on.svg" 
+icon_volume_high="$icon_base/panel/audio-on.svg" 
+icon_volume="$icon_base/panel/audio-volume-high.svg" 
+icon_volume_mid="$icon_base/panel/audio-volume-medium.svg" 
+icon_volume_low="$icon_base/panel/audio-volume-low.svg" 
 icon_mute="$icon_base/panel/audio-volume-muted.svg"
+mic_mute="$icon_base/panel/mic-off.svg"
+mic_on="$icon_base/panel/mic-on.svg"
 
 function get_volume {
         amixer get Master | grep '%' | head -n 1 | cut -d '[' -f 2 | cut -d '%' -f 1
@@ -11,22 +16,31 @@ function is_mute {
         amixer get Master | grep '%' | grep -oE '[^ ]+$' | grep off > /dev/null
 }
 
+function mic_on_off {
+        pactl get-source-mute @DEFAULT_SOURCE@ | grep -Po '(?<=Mute: )(yes|no)' | grep no > /dev/null
+}
+
 function send_notification {
         volume=`get_volume`
         # Make the bar with the special character ─ (it's not dash -)
-        # https://en.wikipedia.org/wiki/Box-drawing_character
-        # bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g') [ -z "$bar" ] && bar="─"
-
         if [ "$volume" -gt 0 ]; then
                 bar=$(seq -s "─" $(($volume / 5)) | sed 's/[0-9]//g')"─"
         else
                 bar=""
         fi
         # Send the notification
-        if [ "$volume" -gt 0 ]; then
+        if [ "$volume" -ge 80 ]; then
+                dunstify -a "System" -i "$icon_volume_high" -r 2593 -u low "Volume: ${volume}%" "$bar" -t 2500
+        elif [ "$volume" -ge 50 ]; then
                 dunstify -a "System" -i "$icon_volume" -r 2593 -u low "Volume: ${volume}%" "$bar" -t 2500
-        else
+
+        elif [ "$volume" -ge 10 ]; then
+                dunstify -a "System" -i "$icon_volume_mid" -r 2593 -u low "Volume: ${volume}%" "$bar" -t 2500
+        elif [ "$volume" -eq 0 ]; then
                 dunstify -a "System" -i "$icon_mute" -t 2500 -r 2593 -u low "Volume: ${volume}%" "$bar" 
+        else
+                dunstify -a "System" -i "$icon_volume_low" -r 2593 -u low "Volume: ${volume}%" "$bar" -t 2500
+
         fi
 }
 
@@ -52,4 +66,13 @@ case $1 in
                         send_notification
                 fi
                 ;;
+        mic)
+                pactl set-source-mute @DEFAULT_SOURCE@ toggle
+                if mic_on_off ; then 
+                        dunstify -a "System" -i "$mic_on" -t 2500 -r 2593 -u low "Active"
+                else
+                        dunstify -a "System" -i "$mic_mute" -t 2500 -r 2593 -u low "Mute"
+                fi
+                ;;
+
 esac
